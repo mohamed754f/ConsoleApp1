c1b316fbd94b4fe446c20c1048798ae0d057e7bf
Mohamed Galal <Mohamed Galal@DESKTOP-28D5B9Q>
2024-07-11 13:26:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer: ");
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Invalid input\nPlease enter integer Number: ");
+            }
+
+            Console.WriteLine($"\nMultiplication table {number}:");
+            for (int j = 1; j <= 12; j++)
+            {
+                Console.WriteLine($"{number} x {j} = {number * j}");
+            }
+        }
+    }
+}
